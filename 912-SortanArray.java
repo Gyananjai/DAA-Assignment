09a4a3d1e8506ba91a0df9061b30c1584957299f
Gaurav Kumar Mishra <mishragaurav0247@gmail.com>
2026-08-08 12:59:21 +0530

    Update 912-SortanArray.java

--- 912-SortanArray.java
+++ 912-SortanArray.java
@@ -1,1 +1,55 @@
+class Solution {
 
+    public int[] sortArray(int[] nums) {
+
+        mergeSort(nums, 0, nums.length - 1);
+
+        return nums;
+    }
+    void mergeSort(int[] arr, int low, int high) {
+        if (low >= high) {
+            return;
+        }
+        int mid = low + (high - low) / 2;
+        mergeSort(arr, low, mid);
+        mergeSort(arr, mid + 1, high);
+        merge(arr, low, mid, high);
+    }
+    void merge(int[] arr, int low, int mid, int high) {
+        int i = low;       
+        int j = mid + 1;   
+        int k = 0;
+        int[] temp = new int[high - low + 1];
+        while (i <= mid && j <= high) {
+            if (arr[i] <= arr[j]) {
+                temp[k] = arr[i];
+                i++;
+            } 
+            else {
+                temp[k] = arr[j];
+                j++;
+            }
+
+            k++;
+        }
+
+        while (i <= mid) {
+            temp[k] = arr[i];
+            i++;
+            k++;
+        }
+
+        while (j <= high) {
+            temp[k] = arr[j];
+            j++;
+            k++;
+        }
+
+        k = 0;
+
+        for (int p = low; p <= high; p++) {
+            arr[p] = temp[k];
+            k++;
+        }
+    }
+}
