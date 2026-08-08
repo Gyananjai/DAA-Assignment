433bfe18f13080caafd01d31b77763608f2c6030
Gaurav Kumar Mishra <mishragaurav0247@gmail.com>
2026-08-08 12:49:43 +0530

    Update 162-FindPeakElement.java

--- 162-FindPeakElement.java
+++ 162-FindPeakElement.java
@@ -1,1 +1,18 @@
+class Solution {
+    public int findPeakElement(int[] nums) {
+        int low = 0;
+        int high = nums.length - 1;
 
+        while (low < high) {
+            int mid = low + (high - low) / 2;
+
+            if (nums[mid] > nums[mid + 1]) {
+                high = mid;      // Peak is at mid or on the left
+            } else {
+                low = mid + 1;   // Peak is on the right
+            }
+        }
+
+        return low;
+    }
+}
